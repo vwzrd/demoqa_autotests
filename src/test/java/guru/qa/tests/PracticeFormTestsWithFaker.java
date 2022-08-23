a01dd7e774f2691a71ddd36eb9797f52465ec464
Maxim_Gavrikov <destrallhumn@gmail.com>
2022-08-23 17:53:33 +0300

    moved all test data to variables

--- src/test/java/guru/qa/tests/PracticeFormTestsWithFaker.java
+++ src/test/java/guru/qa/tests/PracticeFormTestsWithFaker.java
@@ -12,25 +12,22 @@ public class PracticeFormTestsWithFaker extends TestBase {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     Faker faker = new Faker();
-    String firstName,
-            lastName,
-            email,
-            phoneNumber,
-            day,
-            year,
-            address;
-
-
-    @BeforeEach
-    void prepareTestData() {
-        firstName = faker.name().firstName();
-        lastName = faker.name().lastName();
-        email = faker.internet().emailAddress();
-        phoneNumber = faker.phoneNumber().subscriberNumber(10).toString();
-        day = faker.number().numberBetween(1, 30) + "";
-        year = faker.number().numberBetween(1990, 2010) + "";
-        address = faker.address().fullAddress();
-    }
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            phoneNumber = faker.phoneNumber().subscriberNumber(10).toString(),
+            day = faker.number().numberBetween(1, 30) + "",
+            year = faker.number().numberBetween(1990, 2010) + "",
+            address = faker.address().fullAddress(),
+            gender = "Other",
+            month = "August",
+            subject1 = "English",
+            subject2 = "Maths",
+            hobby1 = "Reading",
+            hobby2 = "Sports",
+            photo = "photo.jpg",
+            state = "Haryana",
+            city = "Karnal";
 
     @Test
     void fillPracticeForm() {
@@ -40,30 +37,30 @@ public class PracticeFormTestsWithFaker extends TestBase {
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(email)
-                .setGender("Other")
+                .setGender(gender)
                 .setNumber(phoneNumber)
-                .setBirthday(day, "August", year)
-                .setSubject("English")
-                .setSubject("Maths")
-                .setHobby("Reading")
-                .setHobby("Sports")
-                .setPhoto("photo.jpg")
+                .setBirthday(day, month, year)
+                .setSubject(subject1)
+                .setSubject(subject2)
+                .setHobby(hobby1)
+                .setHobby(hobby2)
+                .setPhoto(photo)
                 .setAddress(address)
-                .setCity("Haryana", "Karnal")
+                .setCity(state, city)
                 .submit();
 
         //Проверяем открывшуюся форму
         registrationFormPage.checkResultsTableVisible()
                 .checkResult("Student Name", firstName + " " + lastName)
                 .checkResult("Student Email", email)
-                .checkResult("Gender", "Other")
+                .checkResult("Gender", gender)
                 .checkResult("Mobile", phoneNumber)
-                .checkResult("Date of Birth", day + " August," + year)
-                .checkResult("Subjects", "English, Maths")
-                .checkResult("Hobbies", "Reading, Sports")
-                .checkResult("Picture", "photo.jpg")
+                .checkResult("Date of Birth", String.format("%s %s,%s", day, month, year))
+                .checkResult("Subjects", String.format("%s, %s", subject1, subject2))
+                .checkResult("Hobbies", String.format("%s, %s", hobby1, hobby2))
+                .checkResult("Picture", photo)
                 .checkResult("Address", address)
-                .checkResult("State and City", "Haryana Karnal");
+                .checkResult("State and City", String.format("%s %s", state, city));
 
     }
 }
